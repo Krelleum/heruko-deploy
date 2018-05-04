6e8e861d92304ccead50d7f18c2f1e581e7d096a
Kevin <kevin.mueller3011@gmx.de>
2018-05-04 15:49:07 +0200

    configvar

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const server = http.createServer(app);
 
 
 
-server.listen(config.port, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+
 
 
 server.listen(process.env.PORT);
-
-
