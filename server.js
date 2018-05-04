17a06bd5ac931b696d81fd93e85309bcfa1caa65
Kevin <kevin.mueller3011@gmx.de>
2018-05-04 15:34:57 +0200

    Port config

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const port = 5000;
 const server = http.createServer(app);
 
 
-server.listen(process.env.PORT);
+// server.listen(process.env.PORT || 5000);
+
+server.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
